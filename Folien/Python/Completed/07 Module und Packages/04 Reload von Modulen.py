33c688adeaae24fd601a9e5bda661b746694e9c7
Matthias Hölzl <tc@xantira.com>
2024-11-08 06:27:23 +0100

    Initial version

---
+++ Folien/Python/Completed/07 Module und Packages/04 Reload von Modulen.py
@@ -0,0 +1,105 @@
+# %% [markdown]
+#
+# <div style="text-align:center; font-size:200%;">
+#  <b>Reload von Modulen</b>
+# </div>
+# <br/>
+# <div style="text-align:center;">Dr. Matthias Hölzl</div>
+# <br/>
+
+# %% [markdown]
+#
+# - Module werden nur einmal geladen und dann im Speicher gehalten
+# - Änderungen an der Datei werden nicht automatisch erkannt
+# - Das ist beim Entwickeln vom Modulen etwas unpaktisch
+
+# %%
+# %load_ext autoreload
+# %autoreload 0
+
+
+# %%
+import my_test_module
+
+# %%
+my_test_module.my_function()
+
+# %%
+my_test_module.my_variable
+
+# %% [markdown]
+#
+# - Was passiert, wenn wir die Datei `my_test_module.py` ändern?
+
+# %%
+my_test_module.my_function()
+
+# %%
+my_test_module.my_variable
+
+# %%
+import my_test_module
+
+# %%
+my_test_module.my_function()
+
+# %%
+my_test_module.my_variable
+
+
+# %% [markdown]
+#
+# ## Automatischer Reload von Modulen mit `importlib.reload`
+#
+# - Das Modul `importlib` bietet die Funktion `reload`
+# - Damit kann ein Modul neu geladen werden
+
+# %%
+import importlib
+
+# %%
+importlib.reload(my_test_module)
+
+# %%
+my_test_module.my_function()
+
+# %%
+my_test_module.my_variable
+
+# %% [markdown]
+#
+# ## Automatischer Reload von Modulen
+#
+# In IPython (und damit Jupyter Notebooks) ist es möglich das automatische Laden
+# von modifizierten Modulen zu aktivieren:
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+import my_test_module
+
+
+# %% [markdown]
+#
+# - Was passiert jetzt, wenn wir die Datei `my_test_module.py` ändern?
+
+# %%
+my_test_module.my_function()
+
+# %%
+my_test_module.my_variable
+
+
+# %% [markdown]
+#
+# In Startup-Skripten kann man syntaktische Warnungen vermeiden, wenn man statt dessen
+# schreibt:
+
+# %%
+try:
+    get_ipython().run_line_magic("load_ext", "autoreload")  # noqa
+    get_ipython().run_line_magic("autoreload", "2")  # noqa
+except NameError:
+    pass
---
+++ Folien/Python/Completed/08 Architektur und Design/01 Einführung in Objektorientierung.py
@@ -0,0 +1,65 @@
+# %% [markdown]
+#
+# <div style="text-align:center; font-size:200%;">
+#  <b>Einführung in Objektorientierung</b>
+# </div>
+# <br/>
+# <div style="text-align:center;">Dr. Matthias Hölzl</div>
+# <br/>
+
+# %% [markdown]
+#
+# - Was ist Objektorientierung?
+# - Warum ist das ein guter Ansatz?
+# - Wie gehen wir vor?
+# - Welche Alternativen gibt es?
+# - Müssen wir uns entscheiden?
+
+# %% [markdown]
+#
+# ## Was ist Objektorientierung?
+#
+# - Objekte
+#   - Daten (Zustand) + Verhalten
+#   - Identität
+# - Klassen
+#   - Beschreibung von Objekten
+# - Kapselung
+# - Vererbung/Polymorphie
+
+# %% [markdown]
+#
+# ## Warum ist das ein guter Ansatz?
+#
+# - "Intuitive" Modellierung von Systemen/Problemen
+#   - **Niedrige Repräsentationslücke**
+# - Strukturierung von Code
+# - **Kontrolle von Abhängigkeiten** (Dependency Inversion)
+# - Wiederverwendung von Code
+
+# %% [markdown]
+#
+# ## Wie gehen wir vor?
+#
+# - Versuchen, die Domäne zu verstehen
+# - Modellieren der Domäne mit Klassen und Assoziationen
+# - Hinzufügen von Verhalten
+# - Iterativ!
+
+# %% [markdown]
+#
+# ## Welche Alternativen gibt es?
+#
+# - Funktionale Programmierung
+# - Prozedurale Programmierung
+# - Datengesteuerte Programmierung
+# - ...
+
+# %% [markdown]
+#
+# ## Müssen wir uns entscheiden?
+#
+# - Nein!
+# - Python unterstützt verschiedene Programmierparadigmen
+# - Verwenden Sie diejenigen, die auf Ihr Problem passen
+# - Aber: Objektorientierung ist ein guter Ansatz für viele Probleme
